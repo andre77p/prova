b21d0bd65e06405850cb1456dc70b68fae3f939c
andre pereira rafel <andre.p.rafael2018@gmail.com>
2022-06-07 17:40:48 -0300

    lago

---
+++ Banco de Dados/dml.sql
@@ -0,0 +1,10 @@
+use prova;
+
+
+insert TB_VILAO (nm_vilao, ds_maldades, bt_super_poder)
+		values ("Coringa", "Nunca tira o sorriso do rosto", true);
+        
+insert TB_VILAO (nm_vilao, ds_maldades, bt_super_poder)
+		values ("Voldemort", "Oque Voldemort não tem de nariz, tem de crueldade", false);
+        
+        select * from TB_VILAO;
